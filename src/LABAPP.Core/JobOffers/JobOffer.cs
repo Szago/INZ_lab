60abd3e36763d2992d687e80bb3d93e5e2a8e981
Szago <jaqb@op.pl>
2024-03-25 14:47:34 +0100

    Dodaj pliki projektów.

---
+++ src/LABAPP.Core/JobOffers/JobOffer.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Domain.Entities;
+using LABAPP.Authorization.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace LABAPP.JobOffers
+{
+    [Table("JOBOFFER")]
+    public class JobOffer : Entity<long>
+    {
+        public string Position { get; set; }
+        public string Body { get; set; }
+        public DateTime ActiveFrom { get; set; }
+        public DateTime ActiveTo { get; set; }
+        public string Localization { get; set; }
+        public string CompanyName { get; set; }
+        public int Salary { get; set; }
+        public string ShortDescription { get; set; }
+        public string ContractType { get; set; }
+
+        [ForeignKey(nameof(CreatedByID))]
+        public User User { get; set; }
+        public long CreatedByID { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
---
+++ src/LABAPP.EntityFrameworkCore/EntityFrameworkCore/LABAPPDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using LABAPP.Authorization.Roles;
+using LABAPP.Authorization.Users;
+using LABAPP.MultiTenancy;
+using LABAPP.JobOffers;
+
+namespace LABAPP.EntityFrameworkCore
+{
+    public class LABAPPDbContext : AbpZeroDbContext<Tenant, Role, User, LABAPPDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public LABAPPDbContext(DbContextOptions<LABAPPDbContext> options)
+            : base(options)
+        {
+        }
+    public DbSet<JobOffer> JobOffers { get; set; }
+    }
+}
